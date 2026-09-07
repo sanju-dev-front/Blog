4d3c47571cd3f2bd2db506a941849132f859fc31
Sanju kumar <sanjutaatdrtcuttack@gmail.com>
2026-09-07 23:30:46 +0530

    basic blog upload

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose=require('mongoose');
 const cookieParser=require('cookie-parser');
 
 const userRoutes=require('./routes/user');
+const blogRoutes=require('./routes/blog');
+
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+const Blog = require('./models/blog');
 
 mongoose.connect('mongodb://localhost:27017/blog').then(()=>{
     console.log('Connected to MongoDB');
@@ -24,12 +27,15 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.join(__dirname,'public')));
 //routes
-app.get('/',(req,res)=>{
-    res.render('home',{ user:req.user});
+app.get('/', async(req,res)=>{
+    const allBlogs=await Blog.find({}).sort({createdAt:-1});
+    console.log('All blogs:', allBlogs);
+    res.render('home',{ user:req.user ,blogs:allBlogs});
 });
 app.use('/user', userRoutes);
-
+app.use('/blog', blogRoutes);
 app.listen(port,()=>{ 
 console.log(`Server is running on port ${port}`)
 })
